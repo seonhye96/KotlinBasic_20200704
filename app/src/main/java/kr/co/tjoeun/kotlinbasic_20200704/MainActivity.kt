340fabf590e51a34f2f647710f1ece073a16373b
황선혜 <ghkdtjs6451@gmail.com>
2020-07-04 16:10:48 +0900

    버튼 눌리면 토스트

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200704/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200704/MainActivity.kt
@@ -2,10 +2,19 @@ package kr.co.tjoeun.kotlinbasic_20200704
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        touchBtn.setOnClickListener {
+//            터치 버튼이 눌리면 실행될 코드를 적는 공간
+            Toast.makeText(this, "버튼 눌림", Toast.LENGTH_SHORT).show()
+        }
+
+
     }
 }
