3c7fc35a3036ff258f0d87b789afe231e1d77491
황선혜 <ghkdtjs6451@gmail.com>
2020-07-05 10:12:01 +0900

    버튼 눌리면 로그캣에 로그 남기기

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200704/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200704/MainActivity.kt
@@ -2,6 +2,7 @@ package kr.co.tjoeun.kotlinbasic_20200704
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity() {
         touchBtn.setOnClickListener {
 //            터치 버튼이 눌리면 실행될 코드를 적는 공간
             Toast.makeText(this, "버튼 눌림", Toast.LENGTH_SHORT).show()
+
+            Log.d("메인화면", "버튼이 눌림")
+
+            Log.e("메인화면", "에러시 로그 찍을때 사용")
+
         }
 
 
