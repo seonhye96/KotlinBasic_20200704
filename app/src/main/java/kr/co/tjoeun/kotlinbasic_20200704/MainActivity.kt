d24fb85fdb7bac7d6fde1399bd90478e93d8b7f6
황선혜 <ghkdtjs6451@gmail.com>
2020-07-05 10:23:30 +0900

    입력한 내용을 버튼 누르면 반영해주기

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200704/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200704/MainActivity.kt
@@ -11,6 +11,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+//        적용하기 버튼 눌린 경우
+        applyBtn.setOnClickListener {
+//            inputEdt에 입력된 내용(text 속성의 값)을 받아와서(get) 변수로 저장
+            val inputContent = inputEdt.text.toString()  // String으로 저장하는 것이 좋음
+            
+            
+//            resultTxt의 내용(text 속성의 값)으로 반영(set)
+            resultTxt.text = inputContent   // 결과는 inputContent로 넣어라
+
+        }
+
         touchBtn.setOnClickListener {
 //            터치 버튼이 눌리면 실행될 코드를 적는 공간
             Toast.makeText(this, "버튼 눌림", Toast.LENGTH_SHORT).show()
